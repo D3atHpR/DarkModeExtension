3324ddf2c963c2ac419f6088dab480c7f7bc49eb
D3atHpR <kastiel103@gmail.com>
2022-05-12 22:29:11 -0300

    Create Dark Mode

---
+++ popup.js
@@ -0,0 +1,29 @@
+if(document.querySelector(".popup")){
+
+const button = document.querySelector(".button");
+const circle = document.querySelector(".circle");
+
+let buttonOn = false;
+
+button.addEventListener("click",()=>{
+    if(!buttonOn){
+        buttonOn = true;
+        circle.style.animation = "moveCircleRight 1s forwards";
+        button.style.animation = "backgroundR 1s forwards";
+        chrome.tabs.executeScript({
+            file: "appOn.js"
+            
+        })
+
+    }
+    else{
+        buttonOn = false;
+        circle.style.animation = "moveCircleLeft 1s forwards";
+        button.style.animation = "backgroundR1 1s forwards";
+        chrome.tabs.executeScript({
+            file: "appOff.js"
+            
+        })
+    }
+})
+}
